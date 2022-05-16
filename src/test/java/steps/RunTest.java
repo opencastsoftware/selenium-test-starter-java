7b524822480ece768d952c1fc660b758f2a2e1ee
Burdis <andy.burdis@yahoo.com>
2022-05-16 11:29:45 +0100

    Added managed webdriver, upgraded cucumber versions, removed unused code

--- src/test/java/steps/RunTest.java
+++ src/test/java/steps/RunTest.java
@@ -1,13 +1,13 @@
 package steps;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/features"},
-        tags = {"@test"},
+        tags = "@test",
         plugin = {"pretty", "html:target/cucumber"}
 )
 public class RunTest {
