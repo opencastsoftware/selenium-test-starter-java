9f587f45b95aa20be742f873ddae6a233ef6a3c1
Andy Burdis <andy.burdis@yahoo.com>
2022-05-16 11:49:27 +0100

    Webdriver manager implementation (#4)
    * Added managed webdriver, upgraded cucumber versions, removed unused code
    
    * Updated README
    
    * Updated README

--- src/test/java/steps/StepDefs.java
+++ src/test/java/steps/StepDefs.java
@@ -1,12 +1,12 @@
 package steps;
 
-import cucumber.api.java8.En;
+import io.cucumber.java8.En;
 import pages.Page;
 
-public class StepDefs extends Page implements En  {
+public class StepDefs extends Page implements En {
 
     public StepDefs() {
-        Given("I go to url (.*)$", (final String url) ->  getUrl(url));
+        Given("I go to url (.*)$", (final String url) -> getUrl(url));
 
     }
 
--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -1,27 +1,16 @@
 package utils;
 
-import org.apache.commons.lang3.SystemUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Driver {
 
     private static WebDriver driver;
 
-    public static WebDriver getDriver(){
+    public static WebDriver getDriver() {
 
-        if(SystemUtils.IS_OS_MAC) {
-            String driverPath = "src/test/java/utils/drivers/chromedriver_mac_91";
-            System.setProperty("webdriver.chrome.driver", driverPath);
-        } else if(SystemUtils.IS_OS_WINDOWS) {
-            String driverPath = "src/test/java/utils/drivers/chromedriver_windows_91.exe";
-            System.setProperty("webdriver.chrome.driver", driverPath);
-        } else if(SystemUtils.IS_OS_LINUX) {
-            String driverPath = "src/test/java/utils/drivers/chromedriver_linux_245";
-            System.setProperty("webdriver.chrome.driver", driverPath);
-        } else {
-            System.out.println("************** OS Not supported **************");
-        }
+        WebDriverManager.chromedriver().setup();
 
         driver = new ChromeDriver();
         driver.manage().deleteAllCookies();
