a7930fafc751d17ddc12a7bff9c1074990bb043f
kerryannekane <kerry-anne.kane@opencastsoftware.com>
2024-09-24 13:03:40 +0100

    addedDriverManager

--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -2,13 +2,14 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Driver {
 
     protected static WebDriver driver = getDriver();
 
     protected static WebDriver getDriver() {
-
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
